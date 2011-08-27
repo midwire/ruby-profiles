97884568ba10266ca3c1474d5dd92827b55e0ef6
Midwire <chris@midwiretech.com>
2011-08-26 19:38:36 -0500

    updated the case_vs_if tests to be more representative

--- case_vs_if.rb
+++ case_vs_if.rb
@@ -4,62 +4,68 @@ $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__)))
 require 'lib/midwire'
 include Midwire
 
+MAX = 100000
+
 ##################################################
-# Collection.each vs. For x vs. While true vs. Until false
+# case vs if-elsif
 
 # BEGIN case_when
 Profiler.time_this("case when using strings") {
-  x = "10"
-  case x
-  when "1"
-    puts "it is #{x}"
-  when "2"
-    puts "it is #{x}"
-  when "3"
-    puts "it is #{x}"
-  when "4"
-    puts "it is #{x}"
-  when "5"
-    puts "it is #{x}"
-  when "6"
-    puts "it is #{x}"
-  when "7"
-    puts "it is #{x}"
-  when "8"
-    puts "it is #{x}"
-  when "9"
-    puts "it is #{x}"
-  when "10"
-    puts "it is #{x}"
+  MAX.times do |i|
+    x = "5"
+    case x
+    when "1"
+      myvar = "it is #{x}"
+    when "2"
+      myvar = "it is #{x}"
+    when "3"
+      myvar = "it is #{x}"
+    when "4"
+      myvar = "it is #{x}"
+    when "5"
+      myvar = "it is #{x}"
+    when "6"
+      myvar = "it is #{x}"
+    when "7"
+      myvar = "it is #{x}"
+    when "8"
+      myvar = "it is #{x}"
+    when "9"
+      myvar = "it is #{x}"
+    when "10"
+      myvar = "it is #{x}"
+    end
   end
 }
 # END case_when
 
 # BEGIN if_elsif_else
 Profiler.time_this("if elsif else using strings") {
-  x = "10"
-  if x == "1"
-    puts "it is #{x}"
-  elsif x ==  "2"
-    puts "it is #{x}"
-  elsif x ==  "3"
-    puts "it is #{x}"
-  elsif x ==  "4"
-    puts "it is #{x}"
-  elsif x ==  "5"
-    puts "it is #{x}"
-  elsif x ==  "6"
-    puts "it is #{x}"
-  elsif x ==  "7"
-    puts "it is #{x}"
-  elsif x ==  "8"
-    puts "it is #{x}"
-  elsif x ==  "9"
-    puts "it is #{x}"
-  elsif x ==  "10"
-    puts "it is #{x}"
-  else
-    puts "it is #{x}"
+  MAX.times do |i|
+    x = "5"
+    if x == "1"
+      myvar = "it is #{x}"
+    elsif x ==  "2"
+      myvar = "it is #{x}"
+    elsif x ==  "3"
+      myvar = "it is #{x}"
+    elsif x ==  "4"
+      myvar = "it is #{x}"
+    elsif x ==  "5"
+      myvar = "it is #{x}"
+    elsif x ==  "6"
+      myvar = "it is #{x}"
+    elsif x ==  "7"
+      myvar = "it is #{x}"
+    elsif x ==  "8"
+      myvar = "it is #{x}"
+    elsif x ==  "9"
+      myvar = "it is #{x}"
+    elsif x ==  "10"
+      myvar = "it is #{x}"
+    else
+      myvar = "it is #{x}"
+    end
   end
 }
 # BEGIN if_elsif_else
@@ -67,86 +73,92 @@ Profiler.time_this("if elsif else using strings") {
 
 # BEGIN case_when_integers
 Profiler.time_this("case when using integers") {
-  x = 10
-  case x
-  when 1
-    puts "it is #{x}"
-  when 2
-    puts "it is #{x}"
-  when 3
-    puts "it is #{x}"
-  when 4
-    puts "it is #{x}"
-  when 5
-    puts "it is #{x}"
-  when 6
-    puts "it is #{x}"
-  when 7
-    puts "it is #{x}"
-  when 8
-    puts "it is #{x}"
-  when 9
-    puts "it is #{x}"
-  when 10
-    puts "it is #{x}"
+  MAX.times do |i|
+    x = 5
+    case x
+    when 1
+      myvar = "it is #{x}"
+    when 2
+      myvar = "it is #{x}"
+    when 3
+      myvar = "it is #{x}"
+    when 4
+      myvar = "it is #{x}"
+    when 5
+      myvar = "it is #{x}"
+    when 6
+      myvar = "it is #{x}"
+    when 7
+      myvar = "it is #{x}"
+    when 8
+      myvar = "it is #{x}"
+    when 9
+      myvar = "it is #{x}"
+    when 10
+      myvar = "it is #{x}"
+    end
   end
 }
 # END case_when_integers
 
 # BEGIN if_elsif_else_integers
 Profiler.time_this("if elsif else using integers") {
-  x = 10
-  if x == 1
-    puts "it is #{x}"
-  elsif x ==  2
-    puts "it is #{x}"
-  elsif x ==  3
-    puts "it is #{x}"
-  elsif x ==  4
-    puts "it is #{x}"
-  elsif x ==  5
-    puts "it is #{x}"
-  elsif x ==  6
-    puts "it is #{x}"
-  elsif x ==  7
-    puts "it is #{x}"
-  elsif x ==  8
-    puts "it is #{x}"
-  elsif x ==  9
-    puts "it is #{x}"
-  elsif x ==  10
-    puts "it is #{x}"
-  else
-    puts "it is #{x}"
+  MAX.times do |i|
+    x = 5
+    if x == 1
+      myvar = "it is #{x}"
+    elsif x ==  2
+      myvar = "it is #{x}"
+    elsif x ==  3
+      myvar = "it is #{x}"
+    elsif x ==  4
+      myvar = "it is #{x}"
+    elsif x ==  5
+      myvar = "it is #{x}"
+    elsif x ==  6
+      myvar = "it is #{x}"
+    elsif x ==  7
+      myvar = "it is #{x}"
+    elsif x ==  8
+      myvar = "it is #{x}"
+    elsif x ==  9
+      myvar = "it is #{x}"
+    elsif x ==  10
+      myvar = "it is #{x}"
+    else
+      myvar = "it is #{x}"
+    end
   end
 }
 # BEGIN if_elsif_else_integers
 
 # BEGIN if_elsif_else_integers2
 Profiler.time_this("if elsif else using integers with ===") {
-  x = 10
-  if x === 1
-    puts "it is #{x}"
-  elsif x ===  2
-    puts "it is #{x}"
-  elsif x ===  3
-    puts "it is #{x}"
-  elsif x ===  4
-    puts "it is #{x}"
-  elsif x ===  5
-    puts "it is #{x}"
-  elsif x ===  6
-    puts "it is #{x}"
-  elsif x ===  7
-    puts "it is #{x}"
-  elsif x ===  8
-    puts "it is #{x}"
-  elsif x ===  9
-    puts "it is #{x}"
-  elsif x ===  10
-    puts "it is #{x}"
-  else
-    puts "it is #{x}"
+  MAX.times do |i|
+    x = 5
+    if x === 1
+      myvar = "it is #{x}"
+    elsif x ===  2
+      myvar = "it is #{x}"
+    elsif x ===  3
+      myvar = "it is #{x}"
+    elsif x ===  4
+      myvar = "it is #{x}"
+    elsif x ===  5
+      myvar = "it is #{x}"
+    elsif x ===  6
+      myvar = "it is #{x}"
+    elsif x ===  7
+      myvar = "it is #{x}"
+    elsif x ===  8
+      myvar = "it is #{x}"
+    elsif x ===  9
+      myvar = "it is #{x}"
+    elsif x ===  10
+      myvar = "it is #{x}"
+    else
+      myvar = "it is #{x}"
+    end
   end
 }
 # BEGIN if_elsif_else_integers2
